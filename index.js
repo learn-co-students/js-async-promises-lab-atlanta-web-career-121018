03563cbf78d93b1831184819a8dede67ffb0af5d
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-01-31 10:07:11 -0500

    done.

--- index.js
+++ index.js
@@ -10,12 +10,31 @@ function askQuestion(){
   return questions[0]
 }
 
-function displayQuestionOnClick(){
-  let btn = document.querySelector('a')
-  return btn.addEventListener('click', () => {
-    toggleTrueAndFalseButtons()
-    askQuestionThenRemoveQuestion(5000)
+function appendQuestion(question){
+  let container = document.querySelector('.question-container')
+  container.innerHTML = question.questionText;
+} 
+
+function askQuestionThen(time){
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(function(resolve){
+    setTimeout(function(){
+      resolve(question)
+    }, time)
   })
+} 
+
+function removeQuestion(){
+  return new Promise(function(){
+    let container = document.querySelector('.question-container')
+    container.innerHTML = ''
+    toggleTrueAndFalseButtons()
+  })
+} 
+
+function askQuestionThenRemoveQuestion(time){
+  return askQuestionThen(time).then(removeQuestion)
 }
 
 function trueAndFalseButtons(){
@@ -32,29 +51,10 @@ function checkQuestion(question, answer){
   question.questionAnswer == answer
 }
 
-function askQuestionThen(time){
-  question = questions[0]
-  appendQuestion(question)
-  return new Promise(function(resolve){
-    setTimeout(function(){
-      resolve(question)
-    }, time)
-  })
-}
-
-function appendQuestion(question){
-  let container = document.querySelector('.question-container')
-  container.innerHTML = question.questionText;
-}
-
-function removeQuestion(){
-  return new Promise(function(){
-    let container = document.querySelector('.question-container')
-    container.innerHTML = ''
+function displayQuestionOnClick(){
+  let btn = document.querySelector('a')
+  return btn.addEventListener('click', () => {
     toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
   })
-}
-
-function askQuestionThenRemoveQuestion(time){
-  return askQuestionThen(time).then(removeQuestion)
 }
